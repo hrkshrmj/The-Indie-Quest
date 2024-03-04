daf015657741925721137d3ae1fe29e7bc8c85ac
Harikesh <harikeshramji.859@gmail.com>
2024-03-04 15:43:14 +0100

    Fundamentals - Lists - Generate characters, 5th edition

---
+++ Fundamentals - Lists - Generate characters, 5th edition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+var random = new Random();
+
+List<int> abilities = [];
+
+for (int i = 0; i < 6; i++)
+{
+    List<int> rolls = [];
+
+    for (int j = 0; j < 4; j++)
+    {
+        rolls.Add(random.Next(1, 7));
+    }
+    Console.WriteLine($"You roll {rolls[0]}, {rolls[1]}, {rolls[2]}, {rolls[3]}");
+    rolls.Sort();
+    rolls.Remove(rolls[0]);
+
+    int ability = rolls[0] + rolls[1] + rolls[2];
+    Console.WriteLine($"The ability score is {ability}");
+    abilities.Add(ability);
+    Console.WriteLine(ability);
+}
+
+abilities.Sort();
+Console.WriteLine($"Your available ability scores are {abilities[0]}, {abilities[1]}, {abilities[2]}, {abilities[3]}, {abilities[4]}, {abilities[5]}.");
+
+
+// roll, add to list, sort, remove lowest
+
