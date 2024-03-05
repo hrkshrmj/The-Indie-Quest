68e67777fc0f1f30986f1ea316782be8443f9543
Harikesh <harikeshramji.859@gmail.com>
2024-03-05 17:53:46 +0100

    Pushing Data Structure missions

---
+++ Data Structures - Arrays - Increasing level difficulty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+class Program
+{
+    private static Random random;
+    static void Main(string[] args)
+    {
+        int[] monsterNum = new int[100];
+        for (int i = 0; i < monsterNum.Length; i++)
+        {
+            random = new Random();
+            int rand = random.Next(1, 51);
+            monsterNum[i] = rand;
+        }
+        Array.Sort(monsterNum);
+        Console.WriteLine($"Number of monsters in levels: {string.Join(",", monsterNum)}" );
+
+    }
+}
---
+++ Data Structures - Arrays - Seasons/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+class Program
+{
+    private static Random random;
+    static void Main(string[] args)
+    {
+        static void CreateDayDescription(int day, int season, int year)
+        {
+            string[] SeasonsArray = { "Spring", "Summer", "Autumn", "Winter" };
+            Console.WriteLine($"Day {day} of {SeasonsArray[season]} in the year {year}");
+        }
+
+        random = new Random();
+        CreateDayDescription((random.Next(1, 366)), (random.Next(0,4)) ,(random.Next(0, 2024)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
