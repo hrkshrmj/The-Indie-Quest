7bcb2b453702864e643458ff4e0aa010e33dac83
Harikesh <harikeshramji.859@gmail.com>
2024-03-14 00:23:01 +0100

    Algorithms Unit 2, Party Shuffle, Factorial and Permutation Party Lists

---
+++ Algorithm Design 2 - Party Dilemma (Factorial)/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Algorithm_Design_2___Party_Dilemma__Factorial_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var list = new List<string> { "Miyamoto","Meier","Kojima","Carmack","Newell","Molyneux","Sawyer" };
+            Console.WriteLine($"Starting participants: {string.Join(",",list)}");
+            WriteAllPermutations(list);
+        }
+
+        static List<string> ShuffleList(List<string> items)
+        {
+            Random random = new Random();
+            for (int i = items.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(items.Count);
+                //(items[j], items[i]) = (items[i], items[j]); used a tuple
+                var temp = items[j];
+                items[j] = items[i];
+                items[i] = temp;
+            }
+            return items;
+        }
+        static double Factorial(double n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (n * Factorial(n - 1));
+            }
+        }
+        static void WriteAllPermutations(List<string> items)
+        {
+            double permutations = Factorial(items.Count);
+            Console.WriteLine("Starting orders:");
+            for (int i = 0; i < permutations; i++)
+            {
+                ShuffleList(items);
+                Console.WriteLine(i + 1 + "." + string.Join(",",items));
+            }
+            
+        }
+    }
+}
---
+++ Algorithm Design 2 - Party Shuffle/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Algorithm_Design_2___Party_Shuffle
+{
+    internal class Program
+    {
+        private static Random random;
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Hari", "Raiza", "Aqil", "Shinja", "Shubham", "Anjum", "Rushabh", "Nikita" };
+           
+            Console.Write("Signed up participants: ");
+            Console.Write(string.Join(", ",names));
+
+            Console.WriteLine();
+            Console.WriteLine("ENTER to randomize");
+            Console.ReadLine();
+
+            static void ShuffleList(List<string> items)
+            {
+                var random = new Random();
+                for (int i = items.Count - 1; i > 0; i--)
+                {
+                    int j = random.Next(items.Count);
+                    //(items[j], items[i]) = (items[i], items[j]); used a touple
+                    var temp = items[j];
+                    items[j] = items[i];
+                    items[i] = temp;                    
+                }
+
+                Console.WriteLine("Generating random order...");
+                Console.WriteLine(string.Join(", ", items));
+            }
+
+            ShuffleList(names);
+
+           
+        }
+    }
+}
