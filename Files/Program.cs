8244c07a82b40b50e2613d8cbdd1140f4d43d33f
Harikesh <harikeshramji.859@gmail.com>
2024-03-13 16:42:53 +0100

    Pushing Files and Switch

---
+++ Files/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+using System.IO;
+namespace Files
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        var playerNamePath = File.Create("playername.txt");
+
+        if (File.Exists("playername.txt"))
+            {
+                var nameRead = File.ReadAllText("playername.txt");
+                Console.WriteLine($"Welcome Back, {nameRead}");
+            }
+        else
+            Console.WriteLine("Welcome to your biggest adventure yet!");
+            Console.WriteLine("What is your name, traveler?");
+            File.WriteAllText("playername.txt", Console.ReadLine());
+
+        }
+    }
+}
---
+++ Switch/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set desired price: ");
+            var price = Console.ReadLine(); // stored as string
+            string[] subs = price.Split(' ', ' ');
+            switch (subs[1])
+            {
+                case "+":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[2]);
+                        double setPrice = x + y;
+                        price = Convert.ToString(setPrice);
+                    }
+                    break;
+
+                case "plus":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[2]);
+                        double setPrice = x + y;
+                        price = Convert.ToString(setPrice);
+                    }
+                    break;
+
+                case "-":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[2]);
+                        double setPrice = x - y;
+                        price = Convert.ToString(setPrice);
+                    }
+                    break;
+
+                case "minus":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[2]);
+                        double setPrice = x - y;
+                        price = Convert.ToString(setPrice);
+                    }
+                    break;
+
+                case "*":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[2]);
+                        double setPrice = x * y;
+                        price = Convert.ToString(setPrice);
+                    }
+                    break;
+
+                case "times":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[2]);
+                        double setPrice = x * y;
+                        price = Convert.ToString(setPrice);
+                    }
+                    break;
+
+                case "/":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[2]);
+                        double setPrice = x / y;
+                        price = Convert.ToString(setPrice);
+                      
+                    }
+                    break;
+
+                case "divided":
+                    {
+                        double x = Convert.ToDouble(subs[0]);
+                        double y = Convert.ToDouble(subs[3]);
+                        double setPrice = x / y;
+                        price = Convert.ToString(setPrice);
+
+                    }
+                    break;
+
+
+            }
+            Console.WriteLine($"The price was set to {price}");
+        }
+    }
+}
